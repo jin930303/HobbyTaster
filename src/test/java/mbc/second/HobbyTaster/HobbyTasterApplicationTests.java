2da4859fe51911bab829e7986a7d0a2c28f5bf66
jin <dbrwls12@gmail.com>
2024-11-14 17:39:09 +0900

    초기 세팅

---
+++ src/test/java/mbc/second/HobbyTaster/HobbyTasterApplicationTests.java
@@ -0,0 +1,13 @@
+package mbc.second.HobbyTaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyTasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
