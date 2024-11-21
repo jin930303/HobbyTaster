8ee3c909ff8a3fa39e5d7168a8e50ecd61e5bdbd
jin <dbrwls12@gmail.com>
2024-11-21 09:41:11 +0900

    클래스입력시 데이터 가져오기 성공

--- src/main/java/mbc/second/HobbyTaster/controller/Class/ClassContorller.java
+++ src/main/java/mbc/second/HobbyTaster/controller/Class/ClassContorller.java
@@ -1,23 +1,39 @@
 package mbc.second.HobbyTaster.controller.Class;
 
+import lombok.extern.slf4j.Slf4j;
 import mbc.second.HobbyTaster.dto.Class.ClassDTO;
 import mbc.second.HobbyTaster.entity.Class.ClassEntity;
+import mbc.second.HobbyTaster.entity.MemberEntity;
 import mbc.second.HobbyTaster.service.Class.ClassService;
+import mbc.second.HobbyTaster.service.Member.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
+@Slf4j
 @Controller
 public class ClassContorller {
 
     @Autowired
     ClassService classService;
 
+    @Autowired
+    MemberService memberService;
+
     @GetMapping(value = "cinput")
-    public String class0(){
+    public String class0(Model mo){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDetails userDetails = (UserDetails)principal;
+        String id = ((UserDetails) principal).getUsername();
+        MemberEntity dto=memberService.findbyid(id);
+        log.info("확인 : "+dto);
+        mo.addAttribute("dto",dto);
 
        return "/class/cinput";
    }
