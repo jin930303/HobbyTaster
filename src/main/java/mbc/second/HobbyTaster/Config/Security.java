8da1446be43b2e9a261bad9bdd0da1505acbbdff
shk0349 <shk0349@naver.com>
2024-11-21 10:35:26 +0900

    회원가입 최종 ver.

--- src/main/java/mbc/second/HobbyTaster/Config/Security.java
+++ src/main/java/mbc/second/HobbyTaster/Config/Security.java
@@ -44,11 +44,11 @@ public class Security {
         http
                 .csrf()
                 .ignoringRequestMatchers("/membersave", "/kakaosave",
-                        "/idcheck", "/nicknamecheck", "/emailcheck", "/phonecheck")  // 특정 경로에서만 CSRF 비활성화
+                        "/idcheck", "/nicknamecheck", "/emailcheck", "/phonecheck","/total_search")  // 특정 경로에서만 CSRF 비활성화
                 .and()
                 .authorizeRequests()
                 .requestMatchers("/", "/main", "/memberinput", "/membersave", "/kakaoinput", "/kakaosave",
-                        "/idcheck", "/nicknamecheck", "/emailcheck", "/phonecheck",
+                        "/idcheck", "/nicknamecheck", "/emailcheck", "/phonecheck","/total_search",
                         "/css/**", "/js/**", "/image/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
